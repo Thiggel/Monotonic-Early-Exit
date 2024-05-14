ceea600e43f7b2a05af47c6313a0de2b277b4f65
OliverSavolainen <oliverssavolainen@gmail.com>
2024-05-14 12:01:41 +0200

    Update modeling_t5.py

--- models/modeling_t5.py
+++ models/modeling_t5.py
@@ -221,7 +221,13 @@ class EffT5Attention(T5Attention):
         if output_attentions:
             outputs = outputs + (attn_weights,)
         if skip_mask is not None and skip_mask.sum().item() != hidden_states.shape[0]:
+            print("attention before")
+            print(ids_restore)
             outputs = outputs + (ids_restore,)
+            print("attention after")
+            print(outputs[-1])
+            print("outputs all")
+            print(outputs)
         return outputs
 
 
@@ -263,7 +269,7 @@ class EffT5LayerSelfAttention(T5LayerSelfAttention):
                 ids_restore = attention_output[-1]
                 attention_output = attention_output[:-1]
 
-                keep_hidden_states, hidden_states, ids_restore = split_tensors_by_mask(hidden_states, skip_mask)
+                keep_hidden_states, hidden_states, _ = split_tensors_by_mask(hidden_states, skip_mask, ids_restore)
                 keep_hidden_states = keep_hidden_states + self.dropout(attention_output[0])
                 hidden_states = restore_tensors_by_mask(keep_hidden_states, hidden_states, ids_restore)   
             outputs = (hidden_states,) + attention_output[1:]  # add attentions if we output them     
