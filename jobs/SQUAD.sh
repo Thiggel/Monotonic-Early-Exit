fda412bef64acfc1322a334b3338a0e2d7a4056b
root <denys.sheremet@gmail.com>
2024-05-21 10:51:27 +0000

    added last three hidden states classifier

--- jobs/SQUAD.sh
+++ jobs/SQUAD.sh
@@ -126,7 +126,35 @@ CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
     --fp16 \
     --deploy_scenario False 
     
-
+# three hidden states classifier
+!CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
+    ../src/run_question_answering.py \
+    --model_name_or_path ../src/model_checkpoints/t5-large \
+    --tokenizer_name t5-large \
+    --do_eval \
+    --dataset_name squad \
+    --context_column context \
+    --question_column question \
+    --answer_column answers \
+    --output_dir ../src/save/squad_t5_large/ \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --save_steps 5475 \
+    --learning_rate 1e-4 \
+    --num_train_epochs 10 \
+    --output_hidden_states_decoder True \
+    --use_early_exit True \
+    --exit_position_temp 4 \
+    --exit_conf_threshold 0.5 \
+    --do_train \
+    --train_meta_cm_head \
+    --num_train_epochs 5 \
+    --exit_conf_type last_three_hiddens_classifier \
+    --fp16 \
+    --deploy_scenario False 
+    
 
 
 # TODO: Set epochs!
