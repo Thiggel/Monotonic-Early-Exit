95ae6357067afd6d310c54329d3e8c249e982105
Filipe Laitenberger <f.laitenberger@gmail.com>
2024-05-22 15:02:37 +0000

    update squad scripts

--- jobs/SQUAD.sh
+++ jobs/SQUAD.sh
@@ -1,4 +1,88 @@
-. environment.sh
+. jobs/environment.sh
+
+# meta
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
+    src/run_question_answering.py \
+    --model_name_or_path checkpoints/SQUAD \
+    --tokenizer_name t5-large \
+    --do_eval \
+    --dataset_name squad \
+    --context_column context \
+    --question_column question \
+    --answer_column answers \
+    --output_dir src/save/squad_t5_large/ \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --save_steps 5475 \
+    --learning_rate 1e-4 \
+    --output_hidden_states_decoder True \
+    --use_early_exit True \
+    --exit_conf_type meta \
+    --exit_position_temp 4 \
+    --exit_conf_threshold 0.7 \
+    --do_train \
+    --train_meta_cm_head \
+    --num_train_epochs 5 \
+    --deploy_scenario False \
+    --max_train_samples 10000
+    
+# three hidden states classifier
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
+    src/run_question_answering.py \
+    --model_name_or_path checkpoints/SQUAD \
+    --tokenizer_name t5-large \
+    --do_eval \
+    --dataset_name squad \
+    --context_column context \
+    --question_column question \
+    --answer_column answers \
+    --output_dir src/save/squad_t5_large/ \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --save_steps 5475 \
+    --learning_rate 1e-4 \
+    --output_hidden_states_decoder True \
+    --use_early_exit True \
+    --exit_position_temp 4 \
+    --exit_conf_threshold 0.7 \
+    --do_train \
+    --train_meta_cm_head \
+    --num_train_epochs 5 \
+    --exit_conf_type last_three_hiddens_classifier \
+    --deploy_scenario False \
+    --max_train_samples 10000
+    
+# recurrent classifier
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
+    src/run_question_answering.py \
+    --model_name_or_path checkpoints/SQUAD \
+    --tokenizer_name t5-large \
+    --do_eval \
+    --dataset_name squad \
+    --context_column context \
+    --question_column question \
+    --answer_column answers \
+    --output_dir src/save/squad_t5_large/ \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --save_steps 5475 \
+    --learning_rate 1e-4 \
+    --output_hidden_states_decoder True \
+    --use_early_exit True \
+    --exit_conf_type recurrent_classifier \
+    --exit_position_temp 4 \
+    --exit_conf_threshold 0.7 \
+    --do_train \
+    --train_meta_cm_head \
+    --num_train_epochs 5 \
+    --deploy_scenario False \
+    --max_train_samples 10000
 
 # no early exit
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
@@ -37,7 +121,7 @@ CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
     --save_steps 5475 \
     --learning_rate 1e-4 \
     --num_train_epochs 5 \
-    --exit_conf_threshold 0.9 \
+    --exit_conf_threshold 0.7 \
     --output_hidden_states_decoder True \
     --intermediate_loss_fn weighted_ce \
     --use_early_exit True \
@@ -61,7 +145,7 @@ CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
     --save_steps 5475 \
     --learning_rate 1e-4 \
     --num_train_epochs 5 \
-    --exit_conf_threshold 0.9 \
+    --exit_conf_threshold 0.7 \
     --output_hidden_states_decoder True \
     --intermediate_loss_fn weighted_ce \
     --use_early_exit True \
@@ -85,93 +169,10 @@ CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
     --save_steps 5475 \
     --learning_rate 1e-4 \
     --num_train_epochs 5 \
-    --exit_conf_threshold 0.9 \
+    --exit_conf_threshold 0.7 \
     --output_hidden_states_decoder True \
     --intermediate_loss_fn weighted_ce \
     --use_early_exit True \
     --exit_conf_type last_three_top_prob_heuristic \
     --exit_position_temp 4
 
-# meta
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
-    src/run_question_answering.py \
-    --model_name_or_path checkpoints/SQUAD \
-    --tokenizer_name t5-large \
-    --do_eval \
-    --dataset_name squad \
-    --context_column context \
-    --question_column question \
-    --answer_column answers \
-    --output_dir src/save/squad_t5_large/ \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 32 \
-    --overwrite_output_dir \
-    --predict_with_generate \
-    --save_steps 5475 \
-    --learning_rate 1e-4 \
-    --output_hidden_states_decoder True \
-    --use_early_exit True \
-    --exit_conf_type meta \
-    --exit_position_temp 4 \
-    --exit_conf_threshold 0.9 \
-    --do_train \
-    --train_meta_cm_head \
-    --num_train_epochs 5 \
-    --deploy_scenario False \
-    --max_train_samples 10000
-    
-# three hidden states classifier
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
-    src/run_question_answering.py \
-    --model_name_or_path checkpoints/SQUAD \
-    --tokenizer_name t5-large \
-    --do_eval \
-    --dataset_name squad \
-    --context_column context \
-    --question_column question \
-    --answer_column answers \
-    --output_dir src/save/squad_t5_large/ \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 16 \
-    --overwrite_output_dir \
-    --predict_with_generate \
-    --save_steps 5475 \
-    --learning_rate 1e-4 \
-    --output_hidden_states_decoder True \
-    --use_early_exit True \
-    --exit_position_temp 4 \
-    --exit_conf_threshold 0.9 \
-    --do_train \
-    --train_meta_cm_head \
-    --num_train_epochs 5 \
-    --exit_conf_type last_three_hiddens_classifier \
-    --deploy_scenario False \
-    --max_train_samples 10000
-    
-# recurrent classifier
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.run --nproc_per_node=1 \
-    src/run_question_answering.py \
-    --model_name_or_path checkpoints/SQUAD \
-    --tokenizer_name t5-large \
-    --do_eval \
-    --dataset_name squad \
-    --context_column context \
-    --question_column question \
-    --answer_column answers \
-    --output_dir src/save/squad_t5_large/ \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 16 \
-    --overwrite_output_dir \
-    --predict_with_generate \
-    --save_steps 5475 \
-    --learning_rate 1e-4 \
-    --output_hidden_states_decoder True \
-    --use_early_exit True \
-    --exit_conf_type recurrent_classifier \
-    --exit_position_temp 4 \
-    --exit_conf_threshold 0.9 \
-    --do_train \
-    --train_meta_cm_head \
-    --num_train_epochs 5 \
-    --deploy_scenario False \
-    --max_train_samples 10000
