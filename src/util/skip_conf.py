7419d5ad5e10a3037c8f4ed6975ecbe5b3b29a34
OliverSavolainen <oliverssavolainen@gmail.com>
2024-05-26 21:58:09 +0200

    Update skip_conf.py

--- src/util/skip_conf.py
+++ src/util/skip_conf.py
@@ -104,7 +104,7 @@ def last_three_top_prob_heuristic(
         increasing_for_3_layers = current_increasing & cache['increasing']
         cache['increasing'] =  current_increasing
 
-    if True in increasing_for_3_layers and current_top_prob > threshold:
+    if increasing_for_3_layers & current_top_prob > threshold:
         print("early exit at:" + str(layer_index))
         return torch.ones(hidden_states.shape[0], device=hidden_states.device), cache
     if layer_index > 22:
