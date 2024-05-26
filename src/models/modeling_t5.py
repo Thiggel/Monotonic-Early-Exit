cb27b2560ba0ce127cc1b088b30bd49ac9ff551e
OliverSavolainen <oliverssavolainen@gmail.com>
2024-05-26 23:12:44 +0200

    Update modeling_t5.py

--- src/models/modeling_t5.py
+++ src/models/modeling_t5.py
@@ -179,7 +179,7 @@ class EffT5Attention(T5Attention):
         # get query states
         query_states = shape(self.q(hidden_states))  # (batch_size, n_heads, seq_length, dim_per_head)
         print(query_states.shape)
-
+        print(key_states)
         # compute scores
         scores = torch.matmul(
             query_states, key_states.transpose(3, 2)
